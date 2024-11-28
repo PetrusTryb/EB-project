6e60ae559f19ff1e643a2b9396c216d5cc284d40
Piotr Trybisz <ptrybisz@gmail.com>
2024-11-28 14:28:13 +0100

    Implement scraping of menu hierarchy

--- scraper/main.py
+++ scraper/main.py
@@ -3,6 +3,10 @@ import os
 import requests
 import re
 from tqdm import tqdm
+from lxml import etree
+import bs4
+
+HIERARCHY_ONLY = True
 
 
 class Scraper:
@@ -15,11 +19,70 @@ class Scraper:
             os.makedirs(results_dir)
         if not os.path.exists(self.images_dir):
             os.makedirs(self.images_dir)
+        self.item_id = 0
+        self.menu_hierarchy = []
+
+    def _id(self):
+        self.item_id += 1
+        return self.item_id
 
     def get_all_collections(self):
         collections = self.get_and_save_one('collections', ['id', 'title', 'handle', 'products_count'],
                                             'collections.json')
         return collections
+
+    def _first_level_categories(self, dom):
+        top_items = dom.xpath('//*[@id="shopify-section-header"]/div[2]/div[2]/div/div/nav/ul/li')
+        for item in top_items:
+            try:
+                handle = item.xpath('./a/@href')[0].split('/')[-1]
+            except IndexError:
+                handle = None
+            self.menu_hierarchy.append({
+                'id': self._id(),
+                'parent_handle': None,
+                'parent_id': None,
+                'handle': handle,
+                'readable_test': item.xpath('./*/*/text()')[0]
+            })
+
+    def _nested_categories(self, dom, top_items, relative_xpath):
+        for i, item in enumerate(top_items):
+            sub_items = dom.xpath(
+                f'//*[@id="shopify-section-header"]/div[2]/div[2]/div/div/nav/ul/li[{1 + i}]/{relative_xpath}')
+            for sub_item in sub_items:
+                try:
+                    handle = sub_item.xpath('./a/@href')[0].split('/')[-1]
+                except IndexError:
+                    handle = None
+                if handle:
+                    print(handle)
+                    self.menu_hierarchy.append({
+                        'id': self._id(),
+                        'parent_handle': self.menu_hierarchy[i]['handle'],
+                        'parent_id': self.menu_hierarchy[i]['id'],
+                        'handle': handle,
+                        'readable_test': sub_item.xpath('./*/text()')[0].strip()
+                    })
+
+    def get_menu_hierarchy(self):
+        webpage = requests.get(self.url)
+        soup = bs4.BeautifulSoup(webpage.text, 'html.parser')
+        dom = etree.HTML(str(soup))
+        self._first_level_categories(dom)
+        top_items = self.menu_hierarchy
+        self._nested_categories(dom, top_items, 'div/ul/li')
+        self._nested_categories(dom, top_items, 'div/*/*/ul/li')
+        with open(f'{self.results_dir}/_menu_hierarchy.json', 'w') as file:
+            if self.pretty_print:
+                file.write(json.dumps(self.menu_hierarchy, indent=2))
+            else:
+                file.write(json.dumps(self.menu_hierarchy))
+        # save as CSV
+        with open(f'{self.results_dir}/_menu_hierarchy.csv', 'w', encoding='UTF-8') as file:
+            file.write('id,parent_handle,handle,human_readable_text\n')
+            for item in self.menu_hierarchy:
+                file.write(f"{item['id']},{item['parent_handle']},{item['handle']},{item['readable_test']}\n")
 
     def get_collection_products(self, collection):
         collection_handle = collection['handle']
@@ -87,7 +150,7 @@ class Scraper:
                         with open(image_filename, 'wb') as image_file:
                             for chunk in response.iter_content(1024):
                                 image_file.write(chunk)
-                        #print(f"Downloaded image: {image_filename}")
+                        # print(f"Downloaded image: {image_filename}")
                         counter += 1
                 except Exception as e:
                     print(f"Failed to download image from {image_url}: {e}")
@@ -96,14 +159,16 @@ class Scraper:
 if __name__ == '__main__':
     scraper = Scraper('https://hobbii.pl', 'results', True)
     collections = scraper.get_all_collections()
-    with tqdm(total=len(collections)) as bar:
-        for collection in collections:
-            scraper.get_collection_products(collection)
-            # Load product data from the saved file
-            products_file = os.path.join('results', f"{collection['handle']}_products.json")
-            with open(products_file, 'r') as file:
-                products = json.load(file)
-            scraper.download_images(products)
-            print(f"Downloaded images for collection: {collection['title']}")
-            bar.update(1)
-        bar.close()
+    scraper.get_menu_hierarchy()
+    if not HIERARCHY_ONLY:
+        with tqdm(total=len(collections)) as bar:
+            for collection in collections:
+                scraper.get_collection_products(collection)
+                # Load product data from the saved file
+                products_file = os.path.join('results', f"{collection['handle']}_products.json")
+                with open(products_file, 'r') as file:
+                    products = json.load(file)
+                scraper.download_images(products)
+                print(f"Downloaded images for collection: {collection['title']}")
+                bar.update(1)
+            bar.close()
