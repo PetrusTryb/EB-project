3bb5a98b7d4cdfbde741e9f9b7de2308d78661cc
Piotr Trybisz <ptrybisz@gmail.com>
2025-01-08 01:11:17 +0100

    Fix: 404 errors in backoffice

--- docker-compose/database-init.sh
+++ docker-compose/database-init.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-
+if [ "$DB_DONT_TOUCH" != "1" ]; then
+echo "DB_DONT_TOUCH is set to 0, initializing database"
 mysql -h "$DB_SERVER" -u "$DB_USER" -p"$DB_PASSWD" <<EOF
 DROP DATABASE IF EXISTS BE_184623;
 CREATE DATABASE BE_184623;
 EOF
 
-mysql -h "$DB_SERVER" -u "$DB_USER" -p"$DB_PASSWD" -D BE_184623 < /seed/prestashop.sql
+mysql -h "$DB_SERVER" -u "$DB_USER" -p"$DB_PASSWD" -D BE_184623 <<EOF
+SET autocommit=0;
+SET unique_checks=0;
+SET foreign_key_checks=0;
+SOURCE /seed/prestashop.sql;
+COMMIT;
+SET autocommit=1;
+SET unique_checks=1;
+SET foreign_key_checks=1;
+UPDATE ps_configuration SET value = '$PS_DOMAIN' WHERE name = 'PS_SHOP_DOMAIN' OR name = 'PS_SHOP_DOMAIN_SSL';
+UPDATE ps_shop_url SET domain = '$PS_DOMAIN' WHERE id_shop_url = 1;
+UPDATE ps_shop_url SET domain_ssl = '$PS_DOMAIN' WHERE id_shop_url = 1;
+EOF
+else
+echo "DB_DONT_TOUCH is set to 1, skipping database initialization"
+fi
+
--- docker-compose/install.sh
+++ docker-compose/install.sh
@@ -88,9 +88,8 @@ if [ ! -f ./config/settings.inc.php ] && [ ! -f ./install.lock ]; then
 
         echo "\n* Launching the installer script..."
         runuser -g www-data -u www-data -- php -d memory_limit=-1 /var/www/html/$PS_FOLDER_INSTALL/index_cli.php \
-        --step="database" --domain="$PS_DOMAIN" --db_server=$DB_SERVER:$DB_PORT --db_name="$DB_NAME" --db_user=$DB_USER \
-        --db_password=$DB_PASSWD --prefix="$DB_PREFIX" --firstname="John" --lastname="Doe" \
-        --password="$ADMIN_PASSWD" --email="$ADMIN_MAIL" --language=$PS_LANGUAGE --country=$PS_COUNTRY \
+        --domain="$PS_DOMAIN" --db_server=$DB_SERVER:$DB_PORT --db_name="$DB_NAME" --db_user=$DB_USER \
+        --db_password=$DB_PASSWD --prefix="$DB_PREFIX" --language=$PS_LANGUAGE --country=$PS_COUNTRY \
         --all_languages=$PS_ALL_LANGUAGES --newsletter=0 --send_email=0 --ssl=$PS_ENABLE_SSL --fixtures=0 \
 		
 
